f58e645910f705e80c1395617eb85a05f069e543
Sofi Lahham <lahhamsofia@gmail.com>
2025-10-05 23:08:26 -0300

    Update __init__.py

--- Streaming/__init__.py
+++ Streaming/__init__.py
@@ -1,6 +1,4 @@
-# Streaming/__init__.py
-from .menu import Menu
-from .arquivo_midia import ArquivoDeMidia
-from .playlist import Playlist
-from .usuarios import Usuario
-from .analises import Analises
+"""
+Pacote Streaming – TG1 (Programação Orientada a Dados)
+Contém todas as classes e módulos do sistema de Streaming de Música.
+"""
